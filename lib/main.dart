db8967ed9635d14346a629ad9b47995c95892dbd
mustafahamzaraza <93825691+mustafahamzaraza@users.noreply.github.com>
2023-09-14 13:50:21 +0530

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -3,24 +3,8 @@ import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:quotationbox/screens/signin.dart';
 
-
-
-void main() async{
-
+void main() {
   runApp(MyApp());
-
-  //  runApp(MyApp());
- // WidgetsFlutterBinding.ensureInitialized();
-  // await Firebase.initializeApp();
-  //
-  // SystemChrome.setPreferredOrientations(
-  //     [DeviceOrientation.portraitUp,DeviceOrientation.portraitDown])
-  //     .then((_) => runApp(
-  //     GetMaterialApp(
-  //         debugShowCheckedModeBanner: false,
-  //         home: MyApp())
-  // )
-  // );
 }
 
 class MyApp extends StatelessWidget {
@@ -39,48 +23,21 @@ class MyHomePage extends StatefulWidget {
 }
 class SplashScreenState extends State<MyHomePage> {
 
-  // late SharedPreferences prefs;
-  // bool? _isFirstRun;
-  // String? xval;
-
   @override
   void initState() {
     super.initState();
     initSharedPref();
   }
 
-
   void initSharedPref() async{
-
-
       Timer(
           Duration(seconds: 5),() async{
-        Navigator.pushReplacement(context, MaterialPageRoute(builder: (context) => SignIn()));
+          Navigator.pushReplacement(context, MaterialPageRoute(builder: (context) => SignIn()
+           )
+          );
+         }
+        );
       }
-      );
-
-
-    // prefs = await SharedPreferences.getInstance();
-    // String? valTok = prefs.getString('token');
-    //
-    // bool? status = prefs.getBool('true');
-    //
-    // if(status == false || valTok == null){
-    //   Timer(
-    //       Duration(seconds: 5),() async{
-    //     Navigator.pushReplacement(context, MaterialPageRoute(builder: (context) => Login()));
-    //   }
-    //   );
-    // }
-    //
-    // else{
-    //   Timer(
-    //       Duration(seconds: 5),() async{
-    //     Navigator.pushReplacement(context, MaterialPageRoute(builder: (context) => HomeScreen()));
-    //   }
-    //   );
-    // }
-  }
 
   @override
   Widget build(BuildContext context) {
@@ -88,9 +45,7 @@ class SplashScreenState extends State<MyHomePage> {
       color: Colors.white,
       height: MediaQuery.of(context).size.height,
       width: MediaQuery.of(context).size.width,
-
       child: Image.asset('images/app_images/splash.png',fit: BoxFit.cover,),
-
     );
   }
 }
